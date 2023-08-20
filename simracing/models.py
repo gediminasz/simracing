7e383f66e2c46c48b629c347ca0bea7769106e55
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2023-08-20 19:17:16 +0300

    Extract strategy evaluation

--- simracing/models.py
+++ simracing/models.py
@@ -2,20 +2,61 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 from simracing.constants import Tyre
+from simracing.io import parse_lap_times
 
 
-def fit_linear_models(lap_time_data, max_stint_length):
-    models = {
+def fit_model(tsv_data, **kwargs):
+    lap_time_data = parse_lap_times(tsv_data)
+
+    tyre_models = {
         compound: _fit_linear_model(lap_times)
         for compound, lap_times in lap_time_data.items()
         if lap_times
     }
 
-    for compound, model in models.items():
+    for compound, model in tyre_models.items():
         colors = {Tyre.RS: "crimson", Tyre.RM: "orange", Tyre.RH: "dimgray"}
-        _plot_model(lap_time_data[compound], model, max_stint_length, colors[compound])
+        _plot_model(
+            lap_time_data[compound], model, kwargs["max_stint_length"], colors[compound]
+        )
 
-    return models
+    return StrategyModel(tyre_models=tyre_models, **kwargs)
+
+
+class StrategyModel:
+    def __init__(
+        self,
+        *,
+        tyre_models: dict,
+        required_compounds: set,
+        max_stint_length: int,
+        pit_cost: float,
+    ):
+        self.tyre_models = tyre_models
+        self.max_stint_length = max_stint_length
+        self.required_compounds = required_compounds
+        self.pit_cost = pit_cost
+
+    def evaluate(self, stints):
+        if stints[0][0] == -1:  # must start on some tyres
+            return float("inf")
+
+        if any(stint_length > self.max_stint_length for _, stint_length in stints):
+            return float("inf")
+
+        used_compounds = set(s[0] for s in stints)
+        if used_compounds & self.required_compounds != self.required_compounds:
+            return float("inf")
+
+        time = 0
+        for compound, stint_length in stints:
+            x = [[i] for i in range(stint_length)]
+            stint_time = sum(self.tyre_models[compound].predict(x))
+            time += stint_time
+
+        time += (len(stints) - 1) * self.pit_cost
+
+        return time
 
 
 def _fit_linear_model(lap_data):
