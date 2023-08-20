c76fc48dc9f7f27e3beb114e064fc58fc342b80f
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2023-08-20 18:48:08 +0300

    Add Tyre enum

---
+++ simracing/constants.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Tyre(Enum):
+    RS = 0
+    RM = 1
+    RH = 2
--- simracing/io.py
+++ simracing/io.py
@@ -1,11 +1,27 @@
 import csv
 from io import StringIO
 
+from simracing.constants import Tyre
+
 
 def parse_lap_times(tsv_input):
-    lap_times = {"RS": [], "RM": [], "RH": []}
-    for line in csv.DictReader(StringIO(tsv_input.strip()), delimiter="\t"):
-        for compound in lap_times.keys():
-            if time := line.get(compound):
-                lap_times[compound].append((int(line["Lap"]), float(time)))
-    return lap_times
+    result = {Tyre.RS: [], Tyre.RM: [], Tyre.RH: []}
+    lines = csv.DictReader(StringIO(tsv_input.strip()), delimiter="\t")
+    for line in lines:
+        for compound in result.keys():
+            if lap_time := line.get(compound.name):
+                result[compound].append((int(line["Lap"]), float(lap_time)))
+    return result
+
+
+example1 = """
+Lap	RS	RM
+2	91.994	92.224
+3	91.580	91.934
+4		91.481
+"""
+assert parse_lap_times(example1) == {
+    Tyre.RS: [(2, 91.994), (3, 91.580)],
+    Tyre.RM: [(2, 92.224), (3, 91.934), (4, 91.481)],
+    Tyre.RH: [],
+}
