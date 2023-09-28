a82c5346e8e90e6743590b2b46c0ad682fe7db93
Gediminas <gediminas.zlatkus@gmail.com>
2023-09-28 23:40:33 +0300

    Update printgt7laps.py

--- tools/printgt7laps.py
+++ tools/printgt7laps.py
@@ -1,4 +1,5 @@
-# Usage example: poetry run python .\tools\printgt7laps.py
+# Usage:
+# poetry run python .\tools\printgt7laps.py
 
 import os
 import sys
@@ -16,15 +17,13 @@ print(latest_file)
 laps = load_laps_from_pickle(latest_file)
 laps = list(reversed(laps))
 
-lap_times = "\n".join(
-    f"{l.number}\t{l.lap_finish_time/1000}"
-    for l in laps
-)
-
 mean_fuel = statistics.mean(l.fuel_consumed for l in laps[1:])
 
-pyperclip.copy(lap_times)
-
-print(lap_times)
+for l in laps:
+    print(f"{l.number}\t{l.lap_finish_time/1000}")
 print()
 print(f"Avarage fuel per lap: {mean_fuel:.1f}")
+
+lap_times = "\n".join(str(l.lap_finish_time/1000) for l in laps)
+pyperclip.copy(lap_times)
+print("Lap times copied to clipboard")
