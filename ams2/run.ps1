bc955b8a8baeee7e999ee3470183bf6ee1180c1f
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2025-03-14 23:40:52 +0200

    Lingering changes

--- ams2/run.ps1
+++ ams2/run.ps1
@@ -1,3 +1,3 @@
-python .\generate_config.py --template_path=server.cfg.template --output_path=server.cfg --vehicle_class=StockCarV8_2024 --track=Londrina_long
+python .\generate_config.py --template_path=server.cfg.template --output_path=server.cfg --vehicle_class=StockCarV8_2024 --track=VeloCittaTD
 start-process steam://rungameid/1066890
 & "C:\Program Files (x86)\Steam\steamapps\common\Automobilista 2 - Dedicated Server\DedicatedServerCmd.exe" --config .\server.cfg
